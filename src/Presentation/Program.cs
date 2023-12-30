75a70c0c710d19376a97e51b5930f4c332eeaaaa
DavidLazarescu <prtnprvtmail@protonmail.com>
2023-12-30 09:47:46 +0100

    Added auto-migrating for selfhosted servers

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -5,6 +5,7 @@ using Azure.Identity;
 using Domain.Entities;
 using Infrastructure.Persistence;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Presentation;
 
 
@@ -49,6 +50,7 @@ using (var scope = app.Services.CreateScope())
 	if (builder.Configuration["LIBRUM_SELFHOSTED"] == "true"){
 		var context = services.GetRequiredService<DataContext>();
     	context.Database.EnsureCreated();
+        context.Database.Migrate();
 	}
 	
     // Configure Logging
